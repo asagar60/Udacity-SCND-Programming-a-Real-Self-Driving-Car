1a24b5ad0a65c53ebd0d1aab20643a8b2b8b95b7
Sandeep Aswathnarayana <saswathnaray@smu.edu>
2020-05-08 17:34:23 +0530

    Update tl_classifier.py

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -2,6 +2,8 @@
 
 from styx_msgs.msg import TrafficLight
 
+import os
+import sys
 import cv2
 import numpy as np
 import tensorflow as tf
@@ -9,43 +11,79 @@ import datetime
 
 
 import rospy
+import time
+import yaml
+import label_map_util
+
+'''
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+'''
 
 class TLClassifier(object):
-    def __init__(self, is_simulation):
-        '''
-        self.image_pub = rospy.Publisher("image_topic_2",Image)
-        self.bridge = CvBridge()
-        '''
+    def __init__(self):
+        #TODO load classifier
+        config_string = rospy.get_param("/traffic_light_config")
+        self.config = yaml.safe_load(config_string)
 
-        if is_simulation:
-            self.MODEL_NAME = 'light_classification/frozen-ssd_inception-simulation'
+        self.detection_graph = None
+        self.num_detections = None
+        self.boxes = None
+        self.scores = None
+        self.classes = None
+
+        self.label_map = None
+        self.category_index = None
+
+        self.MIN_SCORE_THRESHOLD = 0.40
+        self.NUM_CLASSES = 3
+
+        # Grab path to current working directory
+        CWD_PATH = os.getcwd()
+        self.is_real = self.config['is_site']
+        
+        if self.is_real:
+            MODEL_NAME = 'frozen-rcnn_inception-site'
         else:
-            self.MODEL_NAME = 'light_classification/frozen-ssd_inception-site'
+            MODEL_NAME = 'frozen-rcnn_inception-simulation'
+        
+        # path to model and label
+        PATH_TO_CKPT = os.path.join(CWD_PATH, 'light_classification', MODEL_NAME,'frozen_inference_graph.pb')
+        PATH_TO_LABELS = os.path.join(CWD_PATH,'light_classification', MODEL_NAME,'label_map.pbtxt')
 
-        self.PATH_TO_FROZEN_GRAPH = self.MODEL_NAME + '/frozen_inference_graph.pb'
+        # Load the label map
+        self.label_map      = label_map_util.load_labelmap(PATH_TO_LABELS)
+        categories          = label_map_util.convert_label_map_to_categories(self.label_map, max_num_classes=self.NUM_CLASSES, use_display_name=True)
+        self.category_index = label_map_util.create_category_index(categories)
 
-        #load classifier
-        # Load a (frozen) Tensorflow model into memory.
+        # Load the Tensorflow model into memory.
         self.detection_graph = tf.Graph()
         with self.detection_graph.as_default():
             od_graph_def = tf.GraphDef()
-            with tf.gfile.GFile(self.PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+            with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
                 serialized_graph = fid.read()
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
 
-            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-            self.boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
-            self.scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
-            self.classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
-            self.num_detections = self.detection_graph.get_tensor_by_name(
-                'num_detections:0')
- 
-        self.session = tf.Session(graph=self.detection_graph)
-        self.threshold = 0.5
+            self.sess = tf.Session(graph=self.detection_graph)
+        
+        sample = cv2.imread(CWD_PATH + '/sample_load.jpg')
+        
+        image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+
+        image_expanded = np.expand_dims(sample, axis=0)
+        
+        (self.boxes, self.scores, self.classes, self.num_detections) = self.sess.run([detection_boxes, detection_scores, detection_classes, num_detections],\
+            feed_dict={image_tensor: image_expanded})
+
+        rospy.loginfo("  ")
+        rospy.loginfo("  ")
+        rospy.loginfo("Classifier loaded successfully!")
     
 
     def get_classification(self, image):
@@ -58,7 +96,7 @@ class TLClassifier(object):
 
         # convert to rgb image
         #image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-        image_rgb = image
+        #image_rgb = image
 
         '''
 	      # image normalization
@@ -75,36 +113,43 @@ class TLClassifier(object):
         self.image_pub.publish(self.bridge.cv2_to_imgmsg(image_rgb, "rgb8"))
         '''
 
-        with self.detection_graph.as_default():
-            image_expand = np.expand_dims(image_rgb, axis=0)
-            start_classification_t = datetime.datetime.now()
+        #TODO implement light color prediction
 
-            (boxes, scores, classes, num_detections) = self.session.run(
-                [self.boxes, self.scores, self.classes, self.num_detections],
-                feed_dict={self.image_tensor: image_expand})
+        start_time = time.time()
+       
+        # Define input and output tensors (i.e. data) for the object detection classifier
+        # Input tensor is the image
+        image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
 
-            end_classification_t = datetime.datetime.now()
-            elapsed_time = end_classification_t - start_classification_t
+        # Output tensors are the detection boxes, scores, and classes
+        detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
 
-            #print("Classification took:", elapsed_time.total_seconds())
+        # Expand image
+        image_expanded = np.expand_dims(image, axis=0)
+        
+        # Perform the actual detection by running the model with the image as input
+        (self.boxes, self.scores, self.classes, self.num_detections) = self.sess.run([detection_boxes, detection_scores, detection_classes, num_detections],\
+            feed_dict={image_tensor: image_expanded})
+        
+        end_time = time.time()
 
-        boxes = np.squeeze(boxes)
-        scores = np.squeeze(scores)
-        classes = np.squeeze(classes).astype(np.int32)
+        if self.scores[0][0] < self.MIN_SCORE_THRESHOLD:
+            light = TrafficLight.UNKNOWN
+            caption = 'UNKNOWN '
+        elif self.classes[0][0] == 3:
+            light = TrafficLight.GREEN
+            caption = 'Red: ' + str(self.scores[0][0] * 100)[:5] + '%'
+        elif self.classes[0][0] == 1:
+            light = TrafficLight.RED
+            caption = 'Yellow: ' + str(self.scores[0][0] * 100)[:5] + '%'
+        elif self.classes[0][0] == 2:
+            light = TrafficLight.YELLOW
+            caption = 'Green ' + str(self.scores[0][0] * 100)[:5] + '%'
+        else:
+            light = TrafficLight.UNKNOWN
+            caption = 'UNKNOWN '
 
-        #print('Best class: ', classes[0])
-        #print('Best score: ', scores[0])
-
-        if scores[0] > self.threshold:
-            if classes[0] == 1:
-                print('Traffic Light is: GREEN')
-                return TrafficLight.GREEN
-            elif classes[0] == 2:
-                print('Traffic Light is: RED')
-                return TrafficLight.RED
-            elif classes[0] == 3:
-                print('Traffic Light is: YELLOW')
-                return TrafficLight.YELLOW
-
-        return TrafficLight.UNKNOWN
-    
+        return light

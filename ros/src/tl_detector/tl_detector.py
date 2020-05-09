7db9f9b9da4d69f4c43ef9441ff66f2243b10cdf
Arun Sagar <asagar60@gmail.com>
2020-05-09 09:26:49 +0530

    fix: yaml loader fix

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -85,7 +85,7 @@ class TLDetector(object):
         rospy.spin()
         '''
         config_string = rospy.get_param("/traffic_light_config")
-        self.config = yaml.load(config_string)
+        self.config = yaml.load(config_string, yaml.Loader)
 
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
